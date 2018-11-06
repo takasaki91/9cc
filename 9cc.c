0ec15d6da56a45ce2920d457df2f6dcb905caeca
takasaki91 <y.baton@gmail.com>
2018-11-06 16:11:30 +0900

    トークナイザーを追加

--- 9cc.c
+++ 9cc.c
@@ -1,33 +1,99 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
 
+enum {
+    TK_NUM = 256,
+    TK_EOF,
+};
+
+typedef struct {
+    int token_type;
+    int val;//TK_NUMのときの値
+    char *input;//入力時の文字列
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+    int i = 0;
+    while(*p) {
+        if(isspace(*p))
+        {
+            p++;
+            continue;
+        }
+        if(*p == '+' || *p == '-')
+        {
+            tokens[i].token_type = *p;
+            tokens[i].input = p;
+            i++;
+            p++;
+            continue;
+        }
+        if(isdigit(*p))
+        {
+            tokens[i].token_type = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p,&p,10);
+            i++;
+            continue;
+        }
+
+        fprintf(stderr, "トークないずできなかった: %s\n", p);
+        exit(1);
+    }
+    tokens[i].token_type = TK_EOF;
+    tokens[i].input = p;
+}
+
+void error(int i) {
+
+    fprintf(stderr, "予期せぬトークンです: %s\n", tokens[i].input);
+    fprintf(stderr, "予期せぬトークンタイプです: %d \n", tokens[i].token_type);
+    exit(1);
+}
 int main(int argc, char **argv) {
     if(argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
 
-    char *p = argv[1];
+    tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n",strtol(p,&p,10));
     
-    while (*p) {
-        if(*p == '+'){
-            p++;
-            printf("  add rax, %ld\n", strtol(p,&p,10));
-            continue;
-        }
-        if(*p == '-'){
-            p++;
-            printf("  sub rax, %ld\n", strtol(p,&p,10));
-            continue;
-        }
+    if(tokens[0].token_type != TK_NUM) {
+        error(0);
+    }
+    printf("  mov rax, %d\n", tokens[0].val);
 
-        fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-        return 1;
+    int i = 1;
+    while (tokens[i].token_type != TK_EOF) {
+        if(tokens[i].token_type == '+'){
+            i++;
+            if(tokens[i].token_type != TK_NUM)
+            {
+                error(i);
+            }
+            printf("  add rax, %d\n", tokens[i].val);
+            i++;
+            continue;
+        }
+        if(tokens[i].token_type == '-'){
+            i++;
+            if(tokens[i].token_type != TK_NUM)
+            {
+                error(i);
+            }
+            printf("  sub rax, %d\n", tokens[i].val);
+            i++;
+            continue;
+        }
+        error(i);
     }
     printf("  ret\n");
     return 0;
